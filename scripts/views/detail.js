62833b4107432fa6349aa8fc3aa02fa7f1251d37
julubis <julubis@github.com>
2023-10-18 23:26:44 +0700

    add graph in homejs & food detail

--- scripts/views/detail.js
+++ scripts/views/detail.js
@@ -1,3 +1,5 @@
+import UrlParser from '../routes/url-parser.js'
+
 const Detail = {
   async render() {
     document.querySelector('link#css-page').href = "styles/pages/detail.css";
@@ -7,6 +9,23 @@ const Detail = {
   async afterRender() {
     document.querySelectorAll('header nav li>a').forEach(el => el.classList.remove('active'));
     document.querySelector('header nav li>a.food').classList.add('active')
+    
+    const url = UrlParser.parseUrlWithoutCombiner()
+    console.log(url.id)
+    const ingred = document.getElementById('food-ingred')
+
+    const response = await fetch("../../../data/recipes.json");
+    const recipes = await response.json();
+    
+    const {name, image, description, serving, time, direction} = recipes.filter(r => r.id === +url.id)[0]
+    document.getElementById('food-img').src = image;
+    document.getElementById('food-name').textContent = name;
+    document.getElementById('food-desc').textContent = description;
+    document.getElementById('food-serv').textContent = `${serving} serving`;
+    document.getElementById('food-time').textContent = `${time} minutes`;
+    document.getElementById('food-direct').innerHTML = `
+    ${direction.map(direct => `<li>${direct}</li>`).join('')}
+    `
   }
 }
 
--- scripts/views/home.js
+++ scripts/views/home.js
@@ -34,6 +34,51 @@ const Home = {
     //   i++
     // }, 10)
     
+    const ctx = document.getElementById('chart');
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        datasets: [{
+          label: 'Body Weight (Kg)',
+          data: [68.4, 68.2, 67.8, 67.6, 67.6, 67.4],
+          borderWidth: 2,
+          borderColor: '#10b981',
+          backgroundColor: '#ecfdf5',
+          radius: 4,
+          lineTension: 0.4
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+              labels: {
+                  // This more specific font property overrides the global property
+                  font: {
+                      size: 14,
+                      family: "'Poppins', san-serif"
+                  }
+              }
+          }
+        },
+        scales: {
+          y: {
+            ticks: {
+              stepSize: 1
+            },
+            grid: {
+              display: false
+            }
+          },
+          x: {
+            grid: {
+              display: false
+            }
+          }
+        }
+      }
+    });
   }
 }
 
